56969953ade375493f9298db94d1bde01b7037b0
NureAlam68 <nurealam151068@gmail.com>
2025-03-04 16:34:47 +0600

    first typescript

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+// Primitive datatypes
+// 1. string
+let userName = "Nure Aalam";
+console.log(userName);
+// 2. number
+let age = 24;
+console.log(age);
+// 3. boolean
+let isMarried = false;
+console.log(isMarried);
+// 4. null
+let data = null;
+console.log(data);
+// 5. undefined
+let value = undefined;
+console.log(value);
+// Object datatypes
+// 1. Array
+let names = ["Sohag", "Saymon"];
+console.log(names);
+let ages = [24, 23];
+console.log(ages);
+let isMarrieds = [false, false];
+console.log(isMarrieds);
+// 2. Object
+let user = { name: "Nure Alam", age: 24 };
+console.log(user);
+// 3. Tuple
+let person = ["Nure Alam", 24];
+console.log(person);
---
+++ datatypes.ts
@@ -0,0 +1,32 @@
+// Primitive datatypes
+// 1. string
+let userName: string = "Nure Aalam";
+console.log(userName);
+// 2. number
+let age: number = 24;
+console.log(age);
+// 3. boolean
+let isMarried: boolean = false;
+console.log(isMarried);
+// 4. null
+let data: null = null;
+console.log(data);
+// 5. undefined
+let value: undefined = undefined;
+console.log(value);
+
+// Object datatypes
+// 1. Array
+let names: string[] = ["Sohag", "Saymon"];
+console.log(names);
+let ages: number[] = [24, 23];
+console.log(ages);
+let isMarrieds: boolean[] = [false, false];
+console.log(isMarrieds);
+// 2. Object
+let user: { name: string, age: number} = { name: "Nure Alam", age: 24};
+console.log(user);
+// 3. Tuple
+let person: [string, number] = ["Nure Alam", 24];
+console.log(person);
+
