11b6459efc02c85bc281d3b9daee7ce83f50d3cd
NureAlam68 <nurealam151068@gmail.com>
2025-03-23 14:54:14 +0600

    union type

---
+++ union.js
@@ -0,0 +1,8 @@
+"use strict";
+let userAge = 24;
+userAge = '24';
+userAge = true;
+let ageArr = [24, 25, 26, '27', '28', '29', true];
+let day = "Friday";
+day = "Sunday";
+day = "Saturday";
---
+++ union.ts
@@ -0,0 +1,12 @@
+let userAge: number | string | boolean = 24;
+
+userAge = '24';
+userAge = true;
+
+
+let ageArr: (number | string | boolean)[] = [24, 25, 26, '27', '28', '29', true];
+
+let day: 'Friday' | 'Saturday' | 'Sunday' = "Friday";
+
+day = "Sunday";
+day = "Saturday";
