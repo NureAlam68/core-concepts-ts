e497c28adc61edaa713f7301d979c56300a9a13b
NureAlam68 <nurealam151068@gmail.com>
2025-03-23 15:56:21 +0600

    type narrowing

--- union.js
+++ union.js
@@ -2,7 +2,21 @@
 let userAge = 24;
 userAge = '24';
 userAge = true;
+let numArr1 = ['24', '25', '26', '27', '28', '29', '30'];
+let numArr2 = [24, 25, 26, 27, 28, 29, 30];
 let ageArr = [24, 25, 26, '27', '28', '29', true];
 let day = "Friday";
 day = "Sunday";
 day = "Saturday";
+//  ===================  Type narrowing  ===================
+function sayDetailsMan(name, age) {
+    let currentAge;
+    if (typeof age === "string") {
+        currentAge = Number(age) - 2;
+    }
+    else {
+        currentAge = age - 2;
+    }
+    console.log(`Name: ${name}, Age: ${currentAge}`);
+}
+sayDetailsMan('John', 24);
--- union.ts
+++ union.ts
@@ -4,9 +4,29 @@ userAge = '24';
 userAge = true;
 
 
+let numArr1: string[] | number[] = ['24', '25', '26', '27', '28', '29', '30'];
+let numArr2: string[] | number[] = [24, 25, 26, 27, 28, 29, 30];
+
+
 let ageArr: (number | string | boolean)[] = [24, 25, 26, '27', '28', '29', true];
 
 let day: 'Friday' | 'Saturday' | 'Sunday' = "Friday";
 
 day = "Sunday";
 day = "Saturday";
+
+
+//  ===================  Type narrowing  ===================
+
+function sayDetailsMan(name: string, age: number | string): void {
+    let currentAge;
+
+    if(typeof age === "string") {
+        currentAge = Number(age) - 2;
+    }else {
+        currentAge = age - 2;
+    }
+
+    console.log(`Name: ${name}, Age: ${currentAge}`);
+}
+sayDetailsMan('John', 24);
