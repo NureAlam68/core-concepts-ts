32000ea7b74e4fcb1cf81a7741da2721037d46d0
NureAlam68 <nurealam151068@gmail.com>
2025-03-24 22:22:21 +0600

    get set method

--- jsClass.js
+++ jsClass.js
@@ -1,4 +1,6 @@
 class Computer {
+    #ssd = 512;     //privet property
+    camera = "50px";
     constructor(name, ram) {
         this.name = name;
         this.ram = ram;
@@ -6,7 +8,20 @@ class Computer {
     }
 
     aboutComputer() {
-        console.log(`This is ${this.name} computer and it has ${this.ram} GB of RAM`);
+        console.log(`This is ${this.name} computer and it has ${this.ram} GB of RAM and ${this.#ssd} GB of SSD`);
+        this.#ssdInfo();
+    }
+
+    #ssdInfo() {
+        console.log(`This computer has ${this.#ssd} GB of SSD`);
+    }
+
+    get cameraDetails() {   // get method can be access property like a object property
+        return this.camera;
+    }
+
+    set cameraDetails(px) {
+        this.camera = px;
     }
 }
 
@@ -14,6 +29,10 @@ let hp = new Computer("HP", 16);
 
 hp.aboutComputer();
 console.log(hp); 
+console.log(hp.cameraDetails);  //get method can be access property like a object property
+hp.cameraDetails = "100px";     //set method can be access property like a object property
+console.log(hp.cameraDetails);  //get method can be access property like a object property
+console.log(hp.ssd);      //privet property can't be accessed outside the class
 
 
 class Student {
